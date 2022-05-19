244280da7d4eb8efe841635bca0bd9a7d6b08dcd
onlytrleehieutrungg <trungtran2k01@gmail.com>
2022-05-19 09:46:23 +0700

    change

--- PRN_211/Program.cs
+++ PRN_211/Program.cs
@@ -6,7 +6,7 @@ namespace PRN_211
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
